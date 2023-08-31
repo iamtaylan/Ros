2f6afd79304ccfa99137b17f09931fd5f8f57edc
iamtaylan <ozturktaylan266@gmail.com>
2023-08-31 20:16:14 +0300

    Lenght.msg yayinlandi

--- scripts/ucgen_cizdirme_kod.py
+++ scripts/ucgen_cizdirme_kod.py
@@ -4,46 +4,52 @@
 import rospy
 from geometry_msgs.msg import Twist
 from math import radians
+from ucgen_cizdirme.msg import VelocityAngular, VelocityBreak, VelocityLineer, Length
 
-def draw_triangle():
-    rospy.init_node('draw_triangle_node', anonymous=True)
-    vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    rate = rospy.Rate(10)  
+class Triangle():
+    def __init__(self):
+        rospy.init_node('draw_triangle_node', anonymous=True)
+        vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        rospy.Subscriber("kenar_uzunluk_node", Length, self.mesafeCallBack)
+        rate = rospy.Rate(10)  
 
-    vel_msg = Twist()
+        vel_msg = Twist()
 
-    kenar_uzunlugu = 1.0  
-    angle = 120.0     
+        self.kenar_uzunlugu = 0.0  
+        angle = 120.0     
 
 
-    # for oluşturup kenar sayısı kadar forun dönmesini sağladık böylece üçgen elde edebileceğiz.
-    for i in range(3):
+        # for oluşturup kenar sayısı kadar forun dönmesini sağladık böylece üçgen elde edebileceğiz.
+        for i in range(3):
 
-        # 120* döndürdük
-        vel_msg.linear.x = 0.0
-        vel_msg.angular.z = radians(angle) 
-        vel_publisher.publish(vel_msg)
-        rospy.sleep(1.0)  
+            # 120* döndürdük
+            vel_msg.linear.x = 0.0
+            vel_msg.angular.z = radians(angle) 
+            vel_publisher.publish(vel_msg)
+            rospy.sleep(1.0)  
 
-        # dur
-        vel_msg.angular.z = 0.0
-        vel_publisher.publish(vel_msg)
-        rospy.sleep(1.0)  
+            # dur
+            vel_msg.angular.z = 0.0
+            vel_publisher.publish(vel_msg)
+            rospy.sleep(1.0)  
 
-        # ilerle
-        vel_msg.linear.x = 0.2  
-        vel_msg.angular.z = 0.0
-        vel_publisher.publish(vel_msg)
-        rospy.sleep(kenar_uzunlugu / 0.2)  # T=X/V formülü ile turtlebot3'ün ne kadar süre gitmesi gerektiğini hesapladık ve bu süre bittiği anda durdurduk
+            # ilerle
+            vel_msg.linear.x = 0.2  
+            vel_msg.angular.z = 0.0
+            vel_publisher.publish(vel_msg)
+            rospy.sleep(self.kenar_uzunlugu / 0.2)  # T=X/V formülü ile turtlebot3'ün ne kadar süre gitmesi gerektiğini hesapladık ve bu süre bittiği anda durdurduk
 
-        # dur
-        vel_msg.linear.x = 0.0
-        vel_publisher.publish(vel_msg)
-        rospy.sleep(1.0)  
+            # dur
+            vel_msg.linear.x = 0.0
+            vel_publisher.publish(vel_msg)
+            rospy.sleep(1.0)  
 
-    rospy.spin()
+        rospy.spin()
+
+    def mesafeCallBack(self, mesaj):
+        self.kenar_uzunlugu = mesaj.kenaruzunluk
 
 try:
-    draw_triangle()
+    Triangle()
 except rospy.ROSInterruptException:
     pass
