2b923362ccfddda619d036099e87e31afc1b3147
iamtaylan <ozturktaylan266@gmail.com>
2023-08-31 19:17:16 +0300

    turtlebot3 eskenar ücgen ciziyor, msg dosyalarını olşturmadan önceki commit !!!

--- scripts/ucgen_cizdirme_kod.py
+++ scripts/ucgen_cizdirme_kod.py
@@ -3,23 +3,47 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from ucgen_cizdirme.msg import Angle, Velocity
-from nav_msgs.msg import Odometry
+from math import radians
+
+def draw_triangle():
+    rospy.init_node('draw_triangle_node', anonymous=True)
+    vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    rate = rospy.Rate(10)  
+
+    vel_msg = Twist()
+
+    kenar_uzunlugu = 1.0  
+    angle = 120.0     
 
 
-def hareket():
-    rospy.init_node("triange")
-    pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
-    velocity_message = Twist()
-        
-    velocity_message.angular.z = 2.0
-    pub.publish(velocity_message)
+    # for oluşturup kenar sayısı kadar forun dönmesini sağladık böylece üçgen elde edebileceğiz.
+    for i in range(3):
 
+        # 120* döndürdük
+        vel_msg.linear.x = 0.0
+        vel_msg.angular.z = radians(angle) 
+        vel_publisher.publish(vel_msg)
+        rospy.sleep(1.0)  
 
+        # dur
+        vel_msg.angular.z = 0.0
+        vel_publisher.publish(vel_msg)
+        rospy.sleep(1.0)  
 
+        # ilerle
+        vel_msg.linear.x = 0.2  
+        vel_msg.angular.z = 0.0
+        vel_publisher.publish(vel_msg)
+        rospy.sleep(kenar_uzunlugu / 0.2)  # T=X/V formülü ile turtlebot3'ün ne kadar süre gitmesi gerektiğini hesapladık ve bu süre bittiği anda durdurduk
+
+        # dur
+        vel_msg.linear.x = 0.0
+        vel_publisher.publish(vel_msg)
+        rospy.sleep(1.0)  
+
+    rospy.spin()
 
 try:
-    hareket()
-
-except rospy.ROSInternalException:
-    print("Dugum Sonlandi!!!")
+    draw_triangle()
+except rospy.ROSInterruptException:
+    pass
