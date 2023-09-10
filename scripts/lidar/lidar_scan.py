32235646099655d3c9f8863ffffdc358edf940ec
iamtaylan <ozturktaylan266@gmail.com>
2023-09-10 15:24:44 +0300

    ufak hatalar kaldı(hız mesajı yayınlama)

---
+++ scripts/lidar/lidar_scan.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import rospy
+from ucgen_cizdirme.msg import Laser, Rotate
+from sensor_msgs.msg import LaserScan
+
+class LazerVerisi():
+    def __init__(self):
+        rospy.init_node("lidar")
+        self.pub = rospy.Publisher("cmd_vel",Rotate,queue_size = 10)
+        rospy.Subscriber("scan",LaserScan,self.lazerCallback)
+        rospy.Subscriber("hiz_topic",Rotate,self.VelCallBack)
+        self.hiz_mesaji = Rotate()
+
+        self.hiz_mesaji.linear.x = 0.0
+        rospy.spin()
+    
+    def lazerCallback(self,mesaj):
+        sol_on = list(mesaj.ranges[0:9])
+        sag_on = list(mesaj.ranges[350:359])
+        on = sol_on + sag_on
+        sol = list(mesaj.ranges[80:100])
+        sag = list(mesaj.ranges[260:280])
+        arka = list(mesaj.ranges[170:190])
+        min_on = min(on)
+        min_sol = min(sol)
+        min_sag = min(sag)
+        min_arka = min(arka)
+        print(min_on,min_sol,min_sag,min_arka)
+        if min_on < 1.0:
+            self.hiz_mesaji.linear.x = 0.0
+            self.pub.publish(self.hiz_mesaji)
+        else:
+            self.hiz_mesaji.linear.x = self.hiz_mesaji.linear.x
+            self.pub.publish(self.hiz_mesaji)
+                    
+
+    def VelCallBack(self, hiz_mesaj):
+        rospy.loginfo("Hiz: %s" %hiz_mesaj.linear)
+        self.hiz_mesaji.linear.x = hiz_mesaj.linear
+
+
+nesne = LazerVerisi()
---
+++ scripts/lidar/publisher_lidar.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import rospy
+from ucgen_cizdirme.msg import Rotate,Vector3
+
+# Hız mesajımızı yayınlicağımız düğümü oluşturduk.
+rospy.init_node("publisher_node",anonymous=True)
+pub = rospy.Publisher("hiz_topic",Rotate,queue_size=10)
+
+rate = rospy.Rate(1) # 1 hz
+
+# Hız değerimizi kullanıcıdan istedik 
+# istediğimiz aralıkta girene kadar while döndürdük
+i = 1
+while i:
+    hiz = float(input("hiz değeri giriniz(örn: 2.0)(maksimum 5.0 giriniz): \n"))
+    if hiz <= 5.0:
+        i = 0
+    else:
+        print("Lütfen 0'dan büyük ve 5.0'dan küçük bir sayı giriniz: ")
+        i = 1
+
+
+
+while not rospy.is_shutdown():
+    rospy.loginfo(hiz)
+    pub.publish(hiz,0)
+    rate.sleep()
+
