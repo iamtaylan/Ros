07a3b5ee2a204a2ab5f97920776ca58e3e1a9fef
iamtaylan <ozturktaylan266@gmail.com>
2023-09-10 15:45:52 +0300

    Clean Code

--- scripts/lidar/lidar_scan.py
+++ scripts/lidar/lidar_scan.py
@@ -7,27 +7,36 @@ from sensor_msgs.msg import LaserScan
 
 class LazerVerisi():
     def __init__(self):
+
+        #düğümlerimizi oluşturduk
         rospy.init_node("lidar")
         self.pub = rospy.Publisher("cmd_vel",Rotate,queue_size = 10)
         rospy.Subscriber("scan",LaserScan,self.lazerCallback)
         rospy.Subscriber("hiz_topic",Vector3 ,self.VelCallBack)
         self.hiz_mesaji = Rotate()
 
+        #başlangıç olarak hızımızı 0 olarak belirledik bu gelen mesajımıza göre değişecek
         self.hiz_mesaji.linear.x = 0.0
         rospy.spin()
     
     def lazerCallback(self,mesaj):
+        
+        # Lidar sensörümüzün çalışma aralıklarını belirledik
         sol_on = list(mesaj.ranges[0:9])
         sag_on = list(mesaj.ranges[350:359])
         on = sol_on + sag_on
         sol = list(mesaj.ranges[80:100])
         sag = list(mesaj.ranges[260:280])
         arka = list(mesaj.ranges[170:190])
+
+        # Sensör verileini yazdırdık.
         min_on = min(on)
         min_sol = min(sol)
         min_sag = min(sag)
         min_arka = min(arka)
         print(min_on,min_sol,min_sag,min_arka)
+        
+        # Robotumuzun hedefe yakınlığına göre hızını durdurduk ya da ddevam ettirdik
         if min_on < (self.hiz_mesaji.linear.x + 0.5):
             self.hiz_mesaji.linear.x = 0.0
             self.pub.publish(self.hiz_mesaji)
@@ -36,6 +45,7 @@ class LazerVerisi():
             self.pub.publish(self.hiz_mesaji)
                     
 
+    # Publisherımızdan gelen hız mesajımızı aldık.
     def VelCallBack(self, hiz_mesaj):
         rospy.loginfo("Hiz: %s" %hiz_mesaj.x)
         self.hiz_mesaji.linear.x = hiz_mesaj.x
